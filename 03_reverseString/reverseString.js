bcb1d8867588a2f38e5df3bbf2c89f39c17b1bd5
Ryan Seaman <76637624+RPSeaman@users.noreply.github.com>
2022-07-14 14:59:04 -0700

    03 reverse string complete

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    let revStr = "";
+    for (let i = 0; i < string.length; i++) {
+        revStr += string[string.length-1-i];
+    }
+    return revStr;
 };
 
 // Do not edit below this line
