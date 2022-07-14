5a38601c5f59b9f6f8755eb71c0e7cad1f9dde5d
Ryan Seaman <76637624+RPSeaman@users.noreply.github.com>
2022-07-14 14:48:29 -0700

    02 repeat string complete

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    let theString = "";
+    if (num < 0){
+        return "ERROR";
+    }
+    for (i = 0; i < num; i++) {
+        theString += string;
+    }
+    return theString;
 };
 
 // Do not edit below this line
