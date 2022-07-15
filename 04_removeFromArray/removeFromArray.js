ae7fa91186f434f54be8c2ec048c6df1a7322fc4
Ryan Seaman <76637624+RPSeaman@users.noreply.github.com>
2022-07-14 18:19:00 -0700

    04 remove from array complete

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,15 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array, ...removes) {
+    let myArray = array;
+    for (let i = 0; i < removes.length; i++) {
+        var theIDX = myArray.indexOf(removes[i]);
+        if (theIDX == -1){
+            myArray = myArray;
+        }
+        else{
+            myArray.splice(theIDX, 1);
+        }
+    }
+    return myArray;
 };
 
 // Do not edit below this line
