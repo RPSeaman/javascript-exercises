aa141a478979b26243193639be15ed57d0bf6162
Ryan Seaman <76637624+RPSeaman@users.noreply.github.com>
2022-07-15 10:22:55 -0700

    05 sum all complete

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,21 @@
-const sumAll = function() {
+const sumAll = function(intOne, intTwo) {
+    let theCount = 0;
+    let starter = intOne;
+    let ender = intTwo;
+    if (intOne > intTwo){
+            starter = intTwo;
+            ender = intOne;
+    }
+    else if (intOne < 0 || intTwo < 0 || typeof intOne != "number" || typeof intTwo != "number"){
+        return "ERROR"
+    }
 
-};
+    for (let i = starter; i <= ender; i++){
+        theCount += i;
+    }
+
+    return theCount;
+}
 
 // Do not edit below this line
 module.exports = sumAll;
