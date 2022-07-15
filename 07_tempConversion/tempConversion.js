944b2f8027d4280abc2fa52c0142bf6deb1f5885
Ryan Seaman <76637624+RPSeaman@users.noreply.github.com>
2022-07-15 10:43:04 -0700

    07 temp conversion complete

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,13 @@
-const ftoc = function() {
-
+const ftoc = function(theFTemp) {
+  let cTemp = (theFTemp-32) * (5/9);
+  let cTempR = Math.round(cTemp * 10) /10;
+  return cTempR
 };
 
-const ctof = function() {
-
+const ctof = function(theCTemp) {
+  let fTemp = (theCTemp * (9/5)) + 32;
+  let fTempR = Math.round(fTemp * 10) /10;
+  return fTempR
 };
 
 // Do not edit below this line
