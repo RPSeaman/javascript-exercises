59350daf6b3ec57d214ee4c4cc0563f0dfc0396c
Ryan Seaman <76637624+RPSeaman@users.noreply.github.com>
2022-07-27 16:17:14 -0700

    palindromes complete and tested

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,17 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+    var string = string.toLowerCase();
+    var cleanString = "";
+    for (var i = 0; i < string.length; i++) {
+        if (/^[a-zA-Z]+$/.test(string[i]) == true) {
+            cleanString += string[i];
+        }
+    }
+    var backwards = "";
+    for (var i = cleanString.length-1; i >= 0; i--) {
+        backwards = backwards.concat(cleanString[i]);
+    }
+    //return backwards;
+    return backwards === cleanString;
 };
 
 // Do not edit below this line
