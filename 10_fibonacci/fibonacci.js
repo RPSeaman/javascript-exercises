192615b22b254a2a8271679abf0ca6d28599c66d
Ryan Seaman <76637624+RPSeaman@users.noreply.github.com>
2022-07-28 13:05:42 -0700

    fibonacci complete and tested

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,19 @@
-const fibonacci = function() {
-
+const fibonacci = function(theOne) {
+    if (theOne < 0) {
+        return "OOPS";
+    }
+    if (theOne.typeof == "string") {
+        return 1;
+    }
+    if (theOne == 1) {
+        return 1;
+    }
+    if (theOne == 2) {
+        return 1;
+    }
+    if (theOne > 2) {
+        return fibonacci(theOne - 1) + fibonacci(theOne - 2);
+    }
 };
 
 // Do not edit below this line
