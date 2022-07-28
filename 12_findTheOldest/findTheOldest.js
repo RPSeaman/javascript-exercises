1c7b4de723735201ffe367cea0e9e7907a471481
Ryan Seaman <76637624+RPSeaman@users.noreply.github.com>
2022-07-28 14:02:40 -0700

    findTheOldest complete and tested

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,4 +1,21 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+    var oldest = 0;
+    var oldestPerson = {};
+    for (var i = 0; i < people.length; i++) {
+        if (people[i].yearOfDeath === undefined) {
+            var currentTime = new Date()
+            var year = currentTime.getFullYear()
+            if (year - people[i].yearOfBirth > oldest) {
+                oldest = year - people[i].yearOfBirth;
+                oldestPerson = people[i];
+            };
+        };
+        if (people[i].yearOfDeath - people[i].yearOfBirth > oldest) {
+            oldest = people[i].yearOfDeath - people[i].yearOfBirth;
+            oldestPerson = people[i];
+        };
+    };
+    return oldestPerson;
 
 };
 
